aa6ffc6a3f46eb0ca4b56d8e4f5323365f620da5
couchjd <couchjeffrey@yahoo.com>
2021-11-26 21:22:48 -0800

    Minor changes for ImGui test.

--- test_client/main.cpp
+++ test_client/main.cpp
@@ -12,8 +12,58 @@
 
 void testMagery();
 void testActor();
+void testImgui();
 
 int main() 
+{
+
+	testImgui();
+	testMagery();
+	testActor();
+
+	std::getchar();
+	return 0;
+}
+
+void testMagery()
+{
+	std::cout << "TESTING MAGIC SYSTEM" << std::endl;
+	Spell* test_spell = new MagerySpell();
+
+	struct Elements elemantality(100, 99, 98, 97, 96, 95);
+
+	MaterialComponent test_mat_comp(elemantality);
+	SomaticComponent test_som_comp;
+
+	test_spell->addComponent(test_mat_comp);
+	test_spell->addComponent(test_som_comp);
+
+	MaterialComponentVector test_get_mat_comp = test_spell->getMaterialComponents();
+	
+	for (auto& mat_component : test_get_mat_comp)
+	{
+		Elements elements = mat_component.getElementatlity();
+		std::cout << "Component type: " << mat_component.getComponentType() << ": " <<
+			elements.m_air << " " << elements.m_arcane << " " << elements.m_earth << " " <<
+			elements.m_fire << " " << elements.m_nature << " " << elements.m_water << std::endl;
+	}
+}
+
+void testActor()
+{
+	std::cout << std::endl << "TESTING ACTOR" << std::endl;
+	Actor* actor = new DrawableActor();
+
+	sf::Vector2f actor_pos = actor->getPosition();
+	std::cout << "Actor pos X: " << actor_pos.x << " Y:" << actor_pos.y << std::endl;
+	
+	actor->move(sf::Vector2f(32.0f, 32.0f));
+	
+	actor_pos = actor->getPosition();
+	std::cout << "Actor pos X: " << actor_pos.x << " Y:" << actor_pos.y << std::endl;
+}
+
+void testImgui()
 {
 	sf::RenderWindow window(sf::VideoMode(640, 480), "");
 	window.setVerticalSyncEnabled(true);
@@ -70,49 +120,4 @@ int main()
 	}
 
 	ImGui::SFML::Shutdown();
-
-	testMagery();
-	testActor();
-
-	std::getchar();
-	return 0;
-}
-
-void testMagery()
-{
-	std::cout << "TESTING MAGIC SYSTEM" << std::endl;
-	Spell* test_spell = new MagerySpell();
-
-	struct Elements elemantality(100, 99, 98, 97, 96, 95);
-
-	MaterialComponent test_mat_comp(elemantality);
-	SomaticComponent test_som_comp;
-
-	test_spell->addComponent(test_mat_comp);
-	test_spell->addComponent(test_som_comp);
-
-	MaterialComponentVector test_get_mat_comp = test_spell->getMaterialComponents();
-	
-	for (auto& mat_component : test_get_mat_comp)
-	{
-		Elements elements = mat_component.getElementatlity();
-		std::cout << "Component type: " << mat_component.getComponentType() << ": " <<
-			elements.m_air << " " << elements.m_arcane << " " << elements.m_earth << " " <<
-			elements.m_fire << " " << elements.m_nature << " " << elements.m_water << std::endl;
-	}
-}
-
-void testActor()
-{
-	std::cout << std::endl << "TESTING ACTOR" << std::endl;
-	Actor* actor = new DrawableActor();
-
-	sf::Vector2f actor_pos = actor->getPosition();
-	std::cout << "Actor pos X: " << actor_pos.x << " Y:" << actor_pos.y << std::endl;
-	
-	actor->move(sf::Vector2f(32.0f, 32.0f));
-	
-	actor_pos = actor->getPosition();
-	std::cout << "Actor pos X: " << actor_pos.x << " Y:" << actor_pos.y << std::endl;
-
 }
